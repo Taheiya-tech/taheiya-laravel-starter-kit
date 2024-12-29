0da55a5f91bd524ced57c8a54dd1af6c786f7540
Albraa Hesham <albraa@taheiya.sa>
2024-12-29 09:33:26 +0300

    adding service provider to the package

---
+++ src/Providers/StarterKitServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace TaheiyaTech\TaheiyaLaravelStarterKit\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use TaheiyaTech\TaheiyaLaravelStarterKit\App\Console\CreateAllFilters;
+use TaheiyaTech\TaheiyaLaravelStarterKit\App\Console\FilterCommand;
+use TaheiyaTech\TaheiyaLaravelStarterKit\App\Console\MakeApi;
+use TaheiyaTech\TaheiyaLaravelStarterKit\App\Console\MakeDto;
+use TaheiyaTech\TaheiyaLaravelStarterKit\App\Console\MakeService;
+
+class StarterKitServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands(
+                commands: [
+                    CreateAllFilters::class,
+                    FilterCommand::class,
+                    MakeApi::class.
+                    MakeDto::class,
+                    MakeService::class
+                ],
+            );
+        }
+    }
+}
